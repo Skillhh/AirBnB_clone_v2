31bc530d5e1d20009072d799ed408fc87b1386b3
arq-gabo <jgabrielguerral@gmail.com>
2019-12-19 17:49:20 -0500

    [FEATURE]Task_07_file_user

--- models/user.py
+++ models/user.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Imports """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     Class User
-
     email(str): mail address
     password (str): password
     first_name (str): first name
     last_name (str): last name
 
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
