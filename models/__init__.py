a178f94dec5e3abd15bb7c19467737f681435c93
arq-gabo <jgabrielguerral@gmail.com>
2019-12-19 17:05:24 -0500

    [FIX]Task_06

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Importing neccesary modules """
 from models.engine.file_storage import FileStorage
-import os
+from os import getenv
 
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
