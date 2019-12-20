e7fdb52a030e52422fa881e076cd521a89a6906c
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-20 13:13:47 -0500

    [UPDATE]

--- models/review.py
+++ models/review.py
@@ -19,7 +19,8 @@ class Review(BaseModel, Base):
     if "HBNB_TYPE_STORAGE" in environ and environ["HBNB_TYPE_STORAGE"] == "db":
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'))
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id'), nullable=False)
     else:
         place_id = ""
         user_id = ""
