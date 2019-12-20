81ff06fe3bb87418d97d5c5423f9308ac032f03f
arq-gabo <jgabrielguerral@gmail.com>
2019-12-20 16:16:55 -0500

    [FEATURE]Amenity_File

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,10 @@
 Imports
 
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
 
 
 class Amenity(BaseModel):
@@ -14,4 +17,12 @@ class Amenity(BaseModel):
         name (str): level amiability
 
     """
-    name = ""
+    if "HBNB_TYPE_STORAGE" in environ and environ["HBNB_TYPE_STORAGE"] == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128),  nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity')
+    else:
+        name = ""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
