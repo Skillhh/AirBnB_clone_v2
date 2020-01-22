42231587979d7e2c3fe4fe6def536e17dc073216
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-21 23:04:48 -0500

    [ADD] File 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Imports """
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Greeting
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ C is Fun
+    """
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text='is cool'):
+    """ Python is cool
+    """
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    """ n is number
+    """
+    return '{} is a number'.format(escape(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def show_number_template(n):
+    """ n is number
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run()
